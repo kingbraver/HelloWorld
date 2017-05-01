5fbd2e1b0fffa98ac0eebd60dd01aa3b1e01dfb2
Yuuki Tanaka <yu-tanaka@rion.co.jp>
2017-05-01 11:30:58 +0900

    ３つに増やした

--- HelloWorld/Class1.cs
+++ HelloWorld/Class1.cs
@@ -12,6 +12,7 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
             Console.Read();
         }
     }
